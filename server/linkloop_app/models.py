768cd89db81c7a3372d6f6e826c8a731b6fb95e5
Liav <liavt242@gmail.com>
2023-04-02 15:33:28 +0300

    models # removed auto assign user to profile

--- server/linkloop_app/models.py
+++ server/linkloop_app/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator, MaxValueValidator, MinValueValidator, RegexValidator
+from django.core.validators import MinLengthValidator, MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -8,14 +8,6 @@ class Profile(models.Model):
         db_table = "user_profile"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles')
-    profile_name = models.CharField(db_column="profile_name", max_length=128, unique=True, blank=False, null=False,
-                                    default="",
-                                    validators=[
-                                        MinLengthValidator(5),
-                                        RegexValidator(regex=r'^[a-zA-Z0-9_\- ]+$',
-                                                       message="Profile name can only contain letters, numbers,"
-                                                               " underscores, hyphens, and spaces."
-                                                       )])
     profile_picture = models.URLField(verbose_name="profile_pic_url", blank=True, null=True)
     location = models.CharField(db_column="location", blank=False, null=False, max_length=128,
                                 validators=[MinLengthValidator(10)])
@@ -25,11 +17,6 @@ class Profile(models.Model):
     date_of_birth = models.DateField(db_column="date_of_birth", blank=False, null=False)
     created_time = models.DateTimeField(db_column="created_time", auto_now_add=True)
     updated_time = models.DateTimeField(db_column="updated_time", auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if not self.pk and hasattr(self, 'user') and self.user:
-            self.user = self.user if isinstance(self.user, User) else User.objects.first()
-        super(Profile, self).save(*args, **kwargs)
 
 
 class Video(models.Model):
