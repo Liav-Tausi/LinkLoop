eaa855e0b2a9b9b4395abe9a737c2a85593154f2
Liav <liavt242@gmail.com>
2023-04-11 13:21:01 +0300

    view_profile # added profile modal call funcs

--- server/linkloop_app/views/view_profile.py
+++ server/linkloop_app/views/view_profile.py
@@ -15,6 +15,7 @@ from ..serializers.serializer_profile import (
 
 
 class ProfileFilter(django_filters.FilterSet):
+    username = django_filters.CharFilter(field_name='user__username', lookup_expr='iexact')
     first_name = django_filters.CharFilter(field_name='user__first_name', lookup_expr='iexact')
     last_name = django_filters.CharFilter(field_name='user__last_name', lookup_expr='iexact')
     rating = django_filters.NumberFilter(field_name='rating', lookup_expr='exact')
@@ -25,17 +26,25 @@ class ProfileFilter(django_filters.FilterSet):
 
     class Meta:
         model = Profile
-        fields = ['first_name', 'last_name', 'min_rating', 'max_rating', 'location', 'is_rating_null']
+        fields = ['username', 'first_name', 'last_name', 'min_rating', 'max_rating', 'location', 'is_rating_null']
 
 
 class ProfileModelViewSet(ModelViewSet):
     queryset = Profile.objects.all()
-    permission_classes = [IsAuthenticated]
     serializer_class = ProfileSerializer
     authentication_classes = [JWTAuthentication]
     filterset_class = ProfileFilter
+    allowed_methods = ['GET', 'POST', 'DELETE']
 
     def create(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        user_id = request.user.pk
+        profile = Profile.objects.filter(user=user_id).first()
+        if profile:
+            return Response({'detail': 'User already has a profile'}, status=status.HTTP_400_BAD_REQUEST)
+
         data_copy = request.data.copy()
         data_copy["user"] = request.user.pk
         serializer = self.get_serializer(data=data_copy)
@@ -44,6 +53,35 @@ class ProfileModelViewSet(ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def retrieve(self, request, *args, **kwargs):
+        user_id = request.user.pk
+        profile = Profile.objects.filter(user=user_id).first()
+        if not profile:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        partial = kwargs.pop('partial', False)
+        user_id = request.user.pk
+        profile = Profile.objects.filter(user=user_id).first()
+        serializer = self.get_serializer(profile, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        user_id = request.user.pk
+        profile = Profile.objects.filter(user=user_id)
+        if not profile:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        self.perform_destroy(profile)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ProfileImpressionModelViewSet(ModelViewSet):
