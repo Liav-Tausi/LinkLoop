f4c5006fc1a6fa6f28e228d66b4e99267fb04d9d
Liav <liavt242@gmail.com>
2023-04-18 00:49:11 +0300

    url_quals # removing the user one to one url models

--- server/linkloop_app/urls/url_quals.py
+++ server/linkloop_app/urls/url_quals.py
@@ -4,19 +4,11 @@ from ..views.views_quals import (
     SkillsModelViewSet,
     EducationModelViewSet,
     ExperienceModelViewSet,
-    ExperienceUserModelViewSet,
-    EducationUserModelViewSet,
-    SkillsUserModelViewSet
 )
 router = routers.DefaultRouter()
 router.register(r'skills', SkillsModelViewSet, basename='skills')
-router.register(r'skills_user', SkillsUserModelViewSet, basename='skills_user')
-
 router.register(r'education', EducationModelViewSet, basename='education')
-router.register(r'education_user', EducationUserModelViewSet, basename='education_user')
-
 router.register(r'experience', ExperienceModelViewSet, basename='experience')
-router.register(r'experience_user', ExperienceUserModelViewSet, basename='experience_user')
 
 urlpatterns: list = list()
 urlpatterns.extend(router.urls)
