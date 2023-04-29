8ca6c1da43d8a17b7e7f23708fb55c7f121181cd
Liav <liavt242@gmail.com>
2023-04-29 23:56:22 +0300

    view_videos # added fixed main video viewset

--- server/linkloop_app/views/view_videos.py
+++ server/linkloop_app/views/view_videos.py
@@ -1,6 +1,5 @@
 import django_filters
 from rest_framework import status
-from rest_framework.mixins import DestroyModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
