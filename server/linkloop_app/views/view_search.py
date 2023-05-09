b7b7c3894ee0b30da5ec5aa7a7839c36099a685e
Liav <liavt242@gmail.com>
2023-05-09 20:23:40 +0300

    view_search # search query viewset

--- server/linkloop_app/views/view_search.py
+++ server/linkloop_app/views/view_search.py
@@ -1,9 +1,14 @@
 from django.db.models import Q
 from rest_framework import viewsets, status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from ..models import Video, Profile
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from ..models import Video, Profile, UserSearch
 from ..serializers.serializer_profile import ProfileSerializer
+from ..serializers.serializer_search import SearchSerializer
 from ..serializers.serializer_videos import BaseVideoSerializer
+from rest_framework.viewsets import ModelViewSet
 
 
 class SearchViewSet(viewsets.ViewSet):
@@ -11,11 +16,8 @@ class SearchViewSet(viewsets.ViewSet):
         query = request.query_params.get('q', None)
         if not query:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
         videos = Video.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
-
         query_parts = query.split()
-
         if len(query_parts) == 2:
             first_name, last_name = query_parts
             profiles = Profile.objects.filter(
@@ -33,3 +35,24 @@ class SearchViewSet(viewsets.ViewSet):
         video_serializer = BaseVideoSerializer(videos, many=True)
         profile_serializer = ProfileSerializer(profiles, many=True)
         return Response({'videos': video_serializer.data, 'profiles': profile_serializer.data})
+
+
+class SearchQueryViewSet(ModelViewSet):
+    queryset = UserSearch.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = SearchSerializer
+    authentication_classes = [JWTAuthentication]
+    allowed_methods = ['POST']
+
+    def create(self, request, *args, **kwargs):
+        data_copy = request.data.copy()
+        data_copy["user"] = request.user.pk
+        serializer = self.get_serializer(data=data_copy)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+
+
