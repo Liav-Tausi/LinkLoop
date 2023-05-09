350cc26948884c894a67a8bee331df91468c94d9
Liav <liavt242@gmail.com>
2023-05-09 20:22:17 +0300

    url_search # SearchQueryViewSet route

--- server/linkloop_app/urls/url_search.py
+++ server/linkloop_app/urls/url_search.py
@@ -1,8 +1,9 @@
 from rest_framework import routers
-from ..views.view_search import SearchViewSet
+from ..views.view_search import SearchViewSet, SearchQueryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'query', SearchViewSet, basename='search')
+router.register(r'search_query', SearchQueryViewSet, basename='search_query')
 
 urlpatterns: list = list()
 urlpatterns.extend(router.urls)
